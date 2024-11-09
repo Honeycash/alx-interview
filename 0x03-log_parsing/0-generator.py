f3adcf2edd097d7813a8eb617e0327163ab7bba9
honeycash <endurancechery@gmail.com>
2024-11-09 11:08:24 -0800

    Log-Parsing

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-'''A script that generates random HTTP request logs in a consistent format.'''
-
+'''A script that generates random HTTP request logs.
+'''
 import random
 import sys
 import datetime
 from time import sleep
-
 for i in range(10000):
     sleep(random.random())
-    log_line = "{:d}.{:d}.{:d}.{:d} - [{}] \"GET {} HTTP/1.1\" {} {}\n".format(
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET {} {}\" {} {}\n".format(
         random.randint(1, 255),
         random.randint(1, 255),
         random.randint(1, 255),
         random.randint(1, 255),
-        datetime.datetime.now().strftime('%d/%b/%Y:%H:%M:%S -0000'),  # Updated date format
-        '/projects/260',  # Updated path to match the parser's expected format
+        datetime.datetime.now(),
+        '/projects/1216',
+        'HTTP/1.1',
         random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
         random.randint(1, 1024)
-    )
-    sys.stdout.write(log_line)
+    ))
     sys.stdout.flush()
